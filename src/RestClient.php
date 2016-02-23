47138648d53957850ff07110d73b844f75f39c4e
Steit Slings <steit@communication.nl>
2016-02-23 16:33:04 +0100

    PUT, POST and DELETE functions modified

--- src/RestClient.php
+++ src/RestClient.php
@@ -110,6 +110,11 @@ class RestClient
      */
     public function delete($path)
     {
+        // Prevent double // in URL
+        if ((substr($this->baseUrl, -1) == '/') && (substr($path, 0, 1) == '/')) {
+            $path = substr($path, 1);
+        }
+
         try {
             $this->executeRequest('DELETE', $this->baseUrl . $path);
         } catch (ClientException $e) {
@@ -122,7 +127,11 @@ class RestClient
     public function post($path, $data, $parameters = [])
     {
         $parameters['json'] = $data;
-
+        // Prevent double // in URL
+        if ((substr($this->baseUrl, -1) == '/') && (substr($path, 0, 1) == '/')) {
+            $path = substr($path, 1);
+        }
+        
         try {
             return $this->executeRequest('POST', $this->baseUrl . $path, $parameters);
         } catch (ClientException $e) {
@@ -135,6 +144,10 @@ class RestClient
     public function put($path, $data, $parameters = [])
     {
         $parameters['json'] = $data;
+        // Prevent double // in URL
+        if ((substr($this->baseUrl, -1) == '/') && (substr($path, 0, 1) == '/')) {
+            $path = substr($path, 1);
+        }
 
         try {
             return $this->executeRequest('PUT', $this->baseUrl . $path, $parameters);
