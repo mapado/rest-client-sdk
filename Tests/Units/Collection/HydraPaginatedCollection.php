19fa90f9d95db9634c08a56ed45e8a8e09316768
Florent Clerc <florent.clerc@gmail.com>
2016-03-10 16:16:39 +0100

    unit test hydraCollection and hydraPaginatedCollection

---
+++ Tests/Units/Collection/HydraPaginatedCollection.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Mapado\RestClientSdk\Tests\Units\Collection;
+
+use atoum;
+
+/**
+ * SdkClient
+ *
+ * @uses   atoum
+ * @author Florent Clerc <florent.clerc@mapado.com>
+ */
+class HydraPaginatedCollection extends atoum
+{
+
+    /**
+     * testCreateHydraPaginatedCollection
+     *
+     * @access public
+     * @return void
+     */
+    public function testCreateHydraPaginatedCollection()
+    {
+        $json = json_decode(file_get_contents(__DIR__ . '/../../data/ticketing.list.paginated.json'), true);
+
+        $this
+            ->given($collection = new \Mapado\RestClientSdk\Collection\HydraPaginatedCollection($json))
+
+            ->then
+            ->object($collection)
+            ->isInstanceOf('Mapado\RestClientSdk\Collection\HydraPaginatedCollection')
+            ->isInstanceOf('\Iterator')
+            ->hasSize(2)
+            ->and
+            ->integer($collection->getTotalItems())->isEqualTo(6)
+            ->and
+            ->array($collection->toArray())->isEqualTo($json['hydra:member'])
+            ->and
+            ->variable($collection->getFirstPage())->isEqualTo($json['hydra:firstPage'])
+            ->and
+            ->variable($collection->getLastPage())->isEqualTo($json['hydra:lastPage'])
+            ->and
+            ->variable($collection->getNextPage())->isEqualTo($json['hydra:nextPage']);
+    }
+}
